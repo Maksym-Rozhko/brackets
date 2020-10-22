8a820536ed98bcb753469655ed4932e04395bcb8
Maksym <maksymRozhko7@gmail.com>
2020-10-22 19:31:27 +0300

    feat: Brackets
    function check(str, bracketsConfig), for given brackets sequence will return true if it is correct and false otherwise.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const checkArr = [];
+    const brackets = Object.fromEntries(bracketsConfig);
+
+    for (const brack of str.split``) {
+        if (checkArr[0] === brack) {
+          checkArr.shift();
+        } else brackets[brack] ? checkArr.unshift(brackets[brack]) : false;
+    }
+
+    return checkArr.length === 0;
 }
+
+// check('()', [['(', ')']]) // -> true
+// check('((()))()', [['(', ')']]) // -> true
+// check('())(', [['(', ')']]) // -> false
+// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+// check('[(])', [['(', ')'], ['[', ']']]) // -> false
+// check('[]()', [['(', ')'], ['[', ']']]) // -> true
+// check('[]()(', [['(', ')'], ['[', ']']]) // -> false
+
+// special case: opening and closing bracket can be the same :)
+
+// check('||', [['|', '|']]) // -> true
+// check('|()|', [['(', ')'], ['|', '|']]) // -> true
+// check('|(|)', [['(', ')'], ['|', '|']]) // -> false
+// check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true
